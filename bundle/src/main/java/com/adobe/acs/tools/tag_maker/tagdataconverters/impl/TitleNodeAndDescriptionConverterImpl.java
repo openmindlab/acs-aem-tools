b0445c651ebb716d6f4627d63b5e38105f8829af
stefanomessi <stefano.messi@openmindonline.it>
2016-12-02 11:10:22 +0100

    TitleNodeAndDescriptionConverter

---
+++ bundle/src/main/java/com/adobe/acs/tools/tag_maker/tagdataconverters/impl/TitleNodeAndDescriptionConverterImpl.java
@@ -0,0 +1,87 @@
+package com.adobe.acs.tools.tag_maker.tagdataconverters.impl;
+
+import com.adobe.acs.tools.tag_maker.TagData;
+import com.adobe.acs.tools.tag_maker.tagdataconverters.TagDataConverter;
+import org.apache.commons.lang.ArrayUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Properties;
+import org.apache.felix.scr.annotations.Property;
+import org.apache.felix.scr.annotations.Service;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by stefano.messi
+ */
+@Component(
+        label = "ACS AEM Tools - Tag Maker - Title Node Name and description Converter"
+)
+@Properties({
+        @Property(
+                label = "Name",
+                name = TagDataConverter.PROP_NAME,
+                value = TitleNodeAndDescriptionConverterImpl.NAME,
+                propertyPrivate = true
+        ),
+        @Property(
+                label = "Label",
+                name = TagDataConverter.PROP_LABEL,
+                value = TitleNodeAndDescriptionConverterImpl.LABEL,
+                propertyPrivate = true
+        )
+})
+@Service
+public class TitleNodeAndDescriptionConverterImpl implements TagDataConverter {
+
+    public static final String NAME = "acs-commons-title-node-name-and-description";
+
+    public static final String LABEL = "Title {{ node-name | description}}";
+
+    private static final Pattern ACCEPT_PATTERN = Pattern.compile(".+\\{\\{(.+)}}|.+\\{\\{(.+)}}\\{\\{(.+)}}$");
+
+    private static final Pattern PATTERN = Pattern.compile("\\{\\{(.+?)}}$");
+
+
+
+    @Override
+    public final String getLabel() {
+        return LABEL;
+    }
+
+    @Override
+    public final TagData convert(String data) {
+        data = StringUtils.stripToEmpty(data);
+
+        String name;
+        String description = StringUtils.EMPTY;
+
+        final Matcher matcher = PATTERN.matcher(data);
+
+        if (matcher.find() && matcher.groupCount() >= 1) {
+            String[] test = StringUtils.split(StringUtils.stripToEmpty(matcher.group(1)), "|");
+            name = StringUtils.trimToEmpty(test[0]);
+            if (ArrayUtils.getLength(test) == 2) {
+                description = StringUtils.trimToEmpty(test[1]);
+            }
+        } else {
+            return TagData.EMPTY;
+        }
+
+        String title = PATTERN.matcher(data).replaceAll(StringUtils.EMPTY);
+        title = StringUtils.defaultIfEmpty(StringUtils.trimToEmpty(title), name);
+
+        final TagData tagData = new TagData(name);
+        tagData.setTitle(title);
+        tagData.setDescription(description);
+
+        return tagData;
+    }
+
+    @Override
+    public boolean accepts(String data) {
+        final Matcher matcher = ACCEPT_PATTERN.matcher(data);
+        return matcher.matches();
+    }
+}
---
+++ bundle/src/test/java/com/adobe/acs/tools/tag_maker/tagdataconverters/impl/TitleNodeAndDescriptionConverterImplTest.java
@@ -0,0 +1,42 @@
+package com.adobe.acs.tools.tag_maker.tagdataconverters.impl;
+
+import com.adobe.acs.tools.tag_maker.TagData;
+import com.adobe.acs.tools.tag_maker.tagdataconverters.TagDataConverter;
+import junit.framework.TestCase;
+import org.apache.commons.lang.StringUtils;
+
+public class TitleNodeAndDescriptionConverterImplTest extends TestCase {
+
+    TagDataConverter converter = new TitleNodeAndDescriptionConverterImpl();
+
+    public void testConvert() throws Exception {
+
+        String expectedTitle = "Hello World";
+        String expectedName = "hello-world";
+        String expectedDescription = "my-description, my-description2, my-description3";
+
+        TagData actual = converter.convert("Hello World {{ hello-world }}");
+
+        assertEquals(expectedTitle, actual.getTitle());
+        assertEquals(expectedName, actual.getName());
+
+        actual = converter.convert(StringUtils.join(new String[]{"{{ ", expectedName, "}}"}));
+
+        assertEquals(expectedName, actual.getTitle());
+        assertEquals(expectedName, actual.getName());
+
+        actual = converter.convert(StringUtils.join(new String[]{"Hello World {{ ", expectedName, "    |    ", expectedDescription, "}}"}));
+
+        assertEquals(expectedTitle, actual.getTitle());
+        assertEquals(expectedName, actual.getName());
+        assertEquals(expectedDescription, actual.getDescription());
+
+
+        actual = converter.convert(StringUtils.join(new String[]{"{{ ", expectedName, "    |    ", expectedDescription, "}}"}));
+
+        assertEquals(expectedName, actual.getTitle());
+        assertEquals(expectedName, actual.getName());
+        assertEquals(expectedDescription, actual.getDescription());
+    }
+
+}
